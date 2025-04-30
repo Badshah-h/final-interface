bfc403b7603546a6cdda9ea5c3b02e4a6c7b3e77
badshah-h <haroon4.dev@gmail.com>
2025-04-30 07:25:45 +0000

    mock removing

--- src/components/admin/user-management/services/index.ts
+++ src/components/admin/user-management/services/index.ts
@@ -16,21 +16,10 @@ import { MockPermissionService } from "./mock/mockPermissionService";
 import { MockActivityLogService } from "./mock/mockActivityLogService";
 
 // Create service instances
-const userServiceImpl = USE_MOCK_API
-  ? new MockUserService()
-  : new UserService();
-
-const roleServiceImpl = USE_MOCK_API
-  ? new MockRoleService()
-  : new RoleService();
-
-const permissionServiceImpl = USE_MOCK_API
-  ? new MockPermissionService()
-  : new PermissionService();
-
-const activityLogServiceImpl = USE_MOCK_API
-  ? new MockActivityLogService()
-  : new ActivityLogService();
+const userServiceImpl = new UserService();
+const roleServiceImpl = new RoleService();
+const permissionServiceImpl = new PermissionService();
+const activityLogServiceImpl = new ActivityLogService();
 
 // Export service instances
 export const userService = userServiceImpl;
